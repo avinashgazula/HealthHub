b5b368e3cd6b4620fb24b25a5fe6bcf88a568392
avinashgazula <avinashgazula@gmail.com>
2020-06-14 18:53:11 -0300

    path changes to fix build issue

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/health-hub-a2'));
+app.use(express.static(__dirname + '/dist/project'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/health-hub-a2/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/project/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
