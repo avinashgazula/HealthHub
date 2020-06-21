855a68fe777c78fd2db4ad062aeaa8f2dc73e56b
avinashgazula <avinashgazula@gmail.com>
2020-06-21 17:37:30 -0300

    Changed Project name

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/project'));
+app.use(express.static(__dirname + '/dist/healthhub'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/project/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/healthhub/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
