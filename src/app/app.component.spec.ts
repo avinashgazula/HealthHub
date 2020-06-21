7f9e052bee713585a8cd83daad003d175287e0cf
avinashgazula <avinashgazula@gmail.com>
2020-06-21 18:22:41 -0300

    Fixed typos and css in login page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project'`, () => {
+  it(`should have as title 'healthhub'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project');
+    expect(app.title).toEqual('healthhub');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('project app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('healthhub app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project';
+  title = 'healthhub';
   constructor(public router: Router) {
 
   }
